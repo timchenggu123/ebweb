4fac04010de66b56a445aeedb63dcff93988e829
Cheng Gu <c28gu@uwaterloo.ca>
2020-06-02 12:43:14 +0800

    preparing for merge zippling

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var SelfReloadJSON = require('self-reload-json');
+
+var content_experiences = new SelfReloadJSON(__dirname + "/experiences.json")
+var content_projects = new SelfReloadJSON(__dirname + "/projects.json")
+var content_educations = new SelfReloadJSON(__dirname + "/educations.json")
+var content_skills = new SelfReloadJSON(__dirname + "/skills.json")
+var content_interests = new SelfReloadJSON(__dirname + "/interests.json")
+
+var router = express.Router();
+
+router.get('/', function(req, res, next){
+    res.render('index',{
+        experiences:content_experiences,
+        projects:content_projects,
+        educations:content_educations,
+        skills:content_skills,
+        interests:content_interests
+    })
+})
+
+module.exports = router;
